5f86061bce2e41f163cbf15e7325bef5d8c9c3bc
Noel Nondoh <noelnondoh@gmail.com>
2023-08-18 20:34:41 +0000

    Update

--- services.fs
+++ services.fs
@@ -1,4 +1,5 @@
 // Step 1: Install Java
+lsb_release -a // Check Ubuntu version
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 // sudo apt install default-jdk
@@ -214,11 +215,15 @@ sudo apt-get install terraform
 // terraform -help
 
 // Step 12: Installing Ansible and Boto
-sudo apt install software-properties-common--
-sudo add-apt-repository --yes --update ppa:ansible/ansible--
-sudo apt install ansible -y
-sudo apt install python3-boto3
-sudo apt install python3-botocore
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible
+
+// sudo apt install software-properties-common--
+// sudo add-apt-repository --yes --update ppa:ansible/ansible--
+// sudo apt install ansible -y
+// sudo apt install python3-boto3
+// sudo apt install python3-botocore
 
 // Step 13: Free space
 sudo apt-get autoremove
@@ -249,3 +254,174 @@ microk8s dashboard-proxy
 // Start and stop Kubernetes to save battery
 microk8s start 
 microk8s stop
+
+// Step 15: Static IP address assignment
+// To configure your system to use static address assignment, create a netplan configuration in the file /etc/netplan/99_config.yaml.
+// network:
+//   version: 2
+//   renderer: networkd
+//   ethernets:
+//     eth0:
+//       addresses:
+//         - 10.10.10.2/24
+//       routes:
+//         - to: default
+//           via: 10.10.10.1
+//       nameservers:
+//           search: [mydomain, otherdomain]
+//           addresses: [10.10.10.1, 1.1.1.1]
+
+// The configuration can then be applied using the netplan command.
+sudo netplan apply
+
+
+// Step 16: Install Kubernetes Cluster on Ubuntu 20.04
+// Lab setup:
+    //     Machine 1 – K8s-master – 192.168.1.40
+    //     Machine 2 – K8s-node-0 – 192.168.1.41
+    //     Machine 3 – K8s-node-1 – 192.168.1.42
+
+sudo hostnamectl set-hostname "k8s-master"     // Run this command on master node
+sudo hostnamectl set-hostname "k8s-node-0"     // Run this command on node-0
+sudo hostnamectl set-hostname "k8s-node-1"     // Run this command on node-1
+
+// Add the following entries in sudo nano /etc/hosts files on each node,
+    // 192.168.1.40    k8s-master
+    // 192.168.1.41    k8s-node-0
+    // 192.168.1.42    k8s-node-1
+
+// Disable swap and add following kernel module on all the nodes ( master + worker nodes).
+// Disable swap, edit /etc/fstab file and comment out the line which includes entry either swap partition or swap file.
+sudo vi /etc/fstab
+
+sudo swapoff -a
+
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
+
+// Install Containerd Runtime on All Nodes
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+sudo apt install -y containerd.io
+
+// Configure the contianerd using follwing command,
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd && sudo systemctl enable containerd
+sudo systemctl status containerd
+
+// Install Kubectl, kubelet and kubeadm on all nodes
+sudo apt install -y apt-transport-https curl 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/kubernetes-xenial.gpg
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt update $ sudo apt install -y kubelet kubeadm kubectl
+
+
+// Initialize Kubernetes Cluster using kubeadm
+// Login to your master node (k8s-master) and run below ‘kubeadm init‘ command to initialize Kubernetes cluster,
+sudo kubeadm init --control-plane-endpoint=k8s-master
+
+// Once the cluster is initialized successfully, we will get the following output
+// Kubeadm-Init-Ubuntu-20-04
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+// Add Worker Nodes to Cluster
+// If you want to add worker nodes to your Kubernetes cluster, log in to each worker node and run the ‘kubeadm join’ command you saved from Step 5.
+// Copy “kubeadm join” command and paste it on both nodes (worker nodes).
+sudo kubeadm join k8s-master:6443 --token kgiecl.fs4nuhs9nx3g523f --discovery-token-ca-cert-hash sha256:2550ea78e1a6a8b9f93c1e0eaeaa47407c67f9c9a6e4a4865c8c4bc982699ce7
+
+// Join-Worker-Nodes-K8s-Cluster-Ubuntu
+// Now, verify the nodes status from the master node,  run “kubectl get nodes” c
+kubectl get nodes
+
+// Deploy Calico Pod Network Add-on
+// From the master node, run the following command to install Calico pod network add-on,
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml
+Install-Calico-k8s-Ubuntu
+
+kubectl get nodes
+
+// To enable bash completion feature on your master node, execute the followings
+
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+source .bashrc
+
+// Test and Verify Kubernetes Cluster
+kubectl create deployment nginx-web --image=nginx
+kubectl get deployments.apps
+kubectl get  pods
+
+// Let’s scale up the deployment, set replicas as 4. Run the following command,
+kubectl scale --replicas=4 deployment nginx-web
+kubectl get deployments.apps nginx-web
+kubectl describe deployments.apps nginx-web
+kubectl run http-web --image=httpd --port=80
+
+
+// Create a service using beneath command and expose above created pod on port 80,
+kubectl expose pod http-web --name=http-service --port=80 --type=NodePort
+kubectl get service http-service
+
+// Download Metrics Server Manifest
+curl -LO https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+// Download-Metrics-Server-Yaml-file-Curl-Command
+curl https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/high-availability-1.21+.yaml
+
+// Modify Metrics Server Yaml File
+vi components.yaml
+
+// Find the args section under the container section, add the following line:
+- --kubelet-insecure-tls
+
+// Under the spec section, add following parameter,
+hostNetwork: true
+
+// Deploy Metrics Server
+kubectl apply -f components.yaml
+
+// Verify Metrics Server Deployment
+kubectl get pods -n kube-system
+
+// Test Metrics Server Installation
+kubectl top nodes
+
+// To view pods resource utilization of your current namespace or specific namespace, run
+kubectl top pod
+
+kubectl top pod -n kube-system
+
+// Install Secret for DockerHub and PostgreSQL
+kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=vsnondoh --docker-password=Omerta@1988 --docker-email=noelnondoh@gmail.com
+kubectl create secret generic pgpassword --from-literal PGPASSWORD=Omerta@1988
+
+// Verify the secrets
+kubectl get secret regcred --output=yaml
+kubectl get secret
+
+// Install NGINX Ingress Controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
+
+// Copy Deployments Files using SSH
+scp -r \Users\root\source\repos\Mini_CRUD_App\kubernetes\ noel@192.168.1.20:/home/noel/k8s
+
